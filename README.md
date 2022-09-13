8b27112fcbfeabb4c4559c1969a8574581f9b6ed
dupegui334 <davidupegui334@hotmail.com>
2022-09-13 02:49:55 +0000

    Edit README file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cloud9-python
-My repository to post all the python files made in the devOps bootcamp
+# cloud9-python class files
+My repository to post all the python files made in the devOps bootcamp, most of them may look pretty simple, but i used them to make commits and practice with GIT 
+
+## aws-automation
+Create a virtual environment if you want and then install robo3 library to work with Python and AWS instances:
+```python
+pip install robo3
+```
+
+### AWS_instance_status
+Script to know the status and a brief info of your instances. 
--- aws-automation/src/AWS_instance_status.py
+++ aws-automation/src/AWS_instance_status.py
@@ -2,14 +2,14 @@
 
 import boto3 #SDK of AWS for python
 
-AWS_REGION = 'us-east-1' #Our AWS region i North Virginia  
+AWS_REGION = 'us-east-1' #Our AWS region is North Virginia  
 
 EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION) #ec2 its just a name we gave to that
 
 instances = EC2_RESOURCE.instances.all() # Grab all the info from the instances
 
 for instance in instances: #For loop to print all the info 
-    print(f'EC2 instance id: {instance.id}') # Print instanceS ID
+    print(f'EC2 instance id: {instance.id}') # Print instances ID
     #print(f"EC2 instance state: {instance.state}")
     print(f"EC2 instance state: {instance.state['Name']}") # cause we need to write 'Name' we have to use doble quotes outside " ". 
     print(f"EC2 instance state: {instance.tags}") # cause we need to write 'Name' we have to use doble quotes outside " ".
