caefa61f3d7a588d134624986c29e5b9f9451722
dupegui334 <davidupegui334@hotmail.com>
2022-09-14 04:39:27 +0000

    Add ATM file to python excercises

---
+++ src/atm.py
@@ -0,0 +1,40 @@
+def main():
+    continuar = True
+    total = 1000
+    
+    while continuar == True:
+        opcion = input('''Bienvenido al banco BBVA, seleccione entre las opciones:
+        1. Consignar
+        2. Retirar
+        3. Salir
+    ''')
+        if opcion == '1':
+            consignar = int(input('Ingrese valor a consignar\n'))
+            
+            if consignar <= total:
+                total = total - consignar
+                cuenta = input('ingrese numero de cuenta\n')
+                print(f'Se ha consignado ${consignar}, a la cuenta {cuenta}, le quedan ${total}')
+            
+            else:
+                print(f'Saldo insuficiente, solo tiene ${total}')
+                
+        elif opcion == '2':
+            retirar = int(input('Ingrese valor a retirar\n'))
+            if retirar <= total:
+                total = total - retirar
+                print(f'Se ha retirado ${retirar}, le quedan ${total}')
+            else:
+                print(f'Saldo insuficiente, solo tiene ${total}')
+        
+        elif opcion == '3':
+            continuar = False
+            
+        else:
+            print('Seleccione opcion correcta')
+            
+    print('Hasta luego')
+    
+    
+if __name__ == '__main__':
+    main()
